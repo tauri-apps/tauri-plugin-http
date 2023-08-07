6d45e3ecae761f1990ed6366e6138c69775c58d8
Amr Bashir <amr.bashir2015@gmail.com>
2023-08-07 11:34:31 +0000

    feat(http): refactor and improvements (#428)
    Co-authored-by: Lucas Nogueira <lucas@tauri.studio>
    
    Committed via a GitHub action: https://github.com/tauri-apps/plugins-workspace/actions/runs/5784571560
    
    Co-authored-by: lucasfernog <lucasfernog@users.noreply.github.com>

--- dist-js/index.d.ts
+++ dist-js/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * Access the HTTP client written in Rust.
+ * Make HTTP requests with the Rust backend.
  *
  * ## Security
  *
@@ -25,310 +25,31 @@ declare global {
     }
 }
 /**
+ * Options to configure the Rust client used to make fetch requests
+ *
  * @since 2.0.0
  */
-interface Duration {
-    secs: number;
-    nanos: number;
-}
-/**
- * @since 2.0.0
- */
-interface ClientOptions {
+export interface ClientOptions {
     /**
      * Defines the maximum number of redirects the client should follow.
      * If set to 0, no redirects will be followed.
      */
     maxRedirections?: number;
-    connectTimeout?: number | Duration;
+    /** Timeout in milliseconds */
+    connectTimeout?: number;
 }
 /**
- * @since 2.0.0
- */
-declare enum ResponseType {
-    JSON = 1,
-    Text = 2,
-    Binary = 3
-}
-/**
- * @since 2.0.0
- */
-interface FilePart<T> {
-    file: string | T;
-    mime?: string;
-    fileName?: string;
-}
-type Part = string | Uint8Array | FilePart<Uint8Array>;
-/**
- * The body object to be used on POST and PUT requests.
+ * Fetch a resource from the network. It returns a `Promise` that resolves to the
+ * `Response` to that `Request`, whether it is successful or not.
  *
- * @since 2.0.0
- */
-declare class Body {
-    type: string;
-    payload: unknown;
-    /** @ignore */
-    private constructor();
-    /**
-     * Creates a new form data body. The form data is an object where each key is the entry name,
-     * and the value is either a string or a file object.
-     *
-     * By default it sets the `application/x-www-form-urlencoded` Content-Type header,
-     * but you can set it to `multipart/form-data` if the Cargo feature `multipart` is enabled.
-     *
-     * Note that a file path must be allowed in the `fs` scope.
-     * @example
-     * ```typescript
-     * import { Body } from "@tauri-apps/plugin-http"
-     * const body = Body.form({
-     *   key: 'value',
-     *   image: {
-     *     file: '/path/to/file', // either a path or an array buffer of the file contents
-     *     mime: 'image/jpeg', // optional
-     *     fileName: 'image.jpg' // optional
-     *   }
-     * });
-     *
-     * // alternatively, use a FormData:
-     * const form = new FormData();
-     * form.append('key', 'value');
-     * form.append('image', file, 'image.png');
-     * const formBody = Body.form(form);
-     * ```
-     *
-     * @param data The body data.
-     *
-     * @returns The body object ready to be used on the POST and PUT requests.
-     *
-     * @since 2.0.0
-     */
-    static form(data: Record<string, Part> | FormData): Body;
-    /**
-     * Creates a new JSON body.
-     * @example
-     * ```typescript
-     * import { Body } from "@tauri-apps/plugin-http"
-     * Body.json({
-     *   registered: true,
-     *   name: 'tauri'
-     * });
-     * ```
-     *
-     * @param data The body JSON object.
-     *
-     * @returns The body object ready to be used on the POST and PUT requests.
-     *
-     * @since 2.0.0
-     */
-    static json<K extends string | number | symbol, V>(data: Record<K, V>): Body;
-    /**
-     * Creates a new UTF-8 string body.
-     * @example
-     * ```typescript
-     * import { Body } from "@tauri-apps/plugin-http"
-     * Body.text('The body content as a string');
-     * ```
-     *
-     * @param value The body string.
-     *
-     * @returns The body object ready to be used on the POST and PUT requests.
-     *
-     * @since 2.0.0
-     */
-    static text(value: string): Body;
-    /**
-     * Creates a new byte array body.
-     * @example
-     * ```typescript
-     * import { Body } from "@tauri-apps/plugin-http"
-     * Body.bytes(new Uint8Array([1, 2, 3]));
-     * ```
-     *
-     * @param bytes The body byte array.
-     *
-     * @returns The body object ready to be used on the POST and PUT requests.
-     *
-     * @since 2.0.0
-     */
-    static bytes(bytes: Iterable<number> | ArrayLike<number> | ArrayBuffer): Body;
-}
-/** The request HTTP verb. */
-type HttpVerb = "GET" | "POST" | "PUT" | "DELETE" | "PATCH" | "HEAD" | "OPTIONS" | "CONNECT" | "TRACE";
-/**
- * Options object sent to the backend.
- *
- * @since 2.0.0
- */
-interface HttpOptions {
-    method: HttpVerb;
-    url: string;
-    headers?: Record<string, unknown>;
-    query?: Record<string, unknown>;
-    body?: Body;
-    timeout?: number | Duration;
-    responseType?: ResponseType;
-}
-/** Request options. */
-type RequestOptions = Omit<HttpOptions, "method" | "url">;
-/** Options for the `fetch` API. */
-type FetchOptions = Omit<HttpOptions, "url">;
-/** @ignore */
-interface IResponse<T> {
-    url: string;
-    status: number;
-    headers: Record<string, string>;
-    rawHeaders: Record<string, string[]>;
-    data: T;
-}
-/**
- * Response object.
- *
- * @since 2.0.0
- * */
-declare class Response<T> {
-    /** The request URL. */
-    url: string;
-    /** The response status code. */
-    status: number;
-    /** A boolean indicating whether the response was successful (status in the range 200–299) or not. */
-    ok: boolean;
-    /** The response headers. */
-    headers: Record<string, string>;
-    /** The response raw headers. */
-    rawHeaders: Record<string, string[]>;
-    /** The response data. */
-    data: T;
-    /** @ignore */
-    constructor(response: IResponse<T>);
-}
-/**
- * @since 2.0.0
- */
-declare class Client {
-    id: number;
-    /** @ignore */
-    constructor(id: number);
-    /**
-     * Drops the client instance.
-     * @example
-     * ```typescript
-     * import { getClient } from '@tauri-apps/plugin-http';
-     * const client = await getClient();
-     * await client.drop();
-     * ```
-     */
-    drop(): Promise<void>;
-    /**
-     * Makes an HTTP request.
-     * @example
-     * ```typescript
-     * import { getClient } from '@tauri-apps/plugin-http';
-     * const client = await getClient();
-     * const response = await client.request({
-     *   method: 'GET',
-     *   url: 'http://localhost:3003/users',
-     * });
-     * ```
-     */
-    request<T>(options: HttpOptions): Promise<Response<T>>;
-    /**
-     * Makes a GET request.
-     * @example
-     * ```typescript
-     * import { getClient, ResponseType } from '@tauri-apps/plugin-http';
-     * const client = await getClient();
-     * const response = await client.get('http://localhost:3003/users', {
-     *   timeout: 30,
-     *   // the expected response type
-     *   responseType: ResponseType.JSON
-     * });
-     * ```
-     */
-    get<T>(url: string, options?: RequestOptions): Promise<Response<T>>;
-    /**
-     * Makes a POST request.
-     * @example
-     * ```typescript
-     * import { getClient, Body, ResponseType } from '@tauri-apps/plugin-http';
-     * const client = await getClient();
-     * const response = await client.post('http://localhost:3003/users', {
-     *   body: Body.json({
-     *     name: 'tauri',
-     *     password: 'awesome'
-     *   }),
-     *   // in this case the server returns a simple string
-     *   responseType: ResponseType.Text,
-     * });
-     * ```
-     */
-    post<T>(url: string, body?: Body, options?: RequestOptions): Promise<Response<T>>;
-    /**
-     * Makes a PUT request.
-     * @example
-     * ```typescript
-     * import { getClient, Body } from '@tauri-apps/plugin-http';
-     * const client = await getClient();
-     * const response = await client.put('http://localhost:3003/users/1', {
-     *   body: Body.form({
-     *     file: {
-     *       file: '/home/tauri/avatar.png',
-     *       mime: 'image/png',
-     *       fileName: 'avatar.png'
-     *     }
-     *   })
-     * });
-     * ```
-     */
-    put<T>(url: string, body?: Body, options?: RequestOptions): Promise<Response<T>>;
-    /**
-     * Makes a PATCH request.
-     * @example
-     * ```typescript
-     * import { getClient, Body } from '@tauri-apps/plugin-http';
-     * const client = await getClient();
-     * const response = await client.patch('http://localhost:3003/users/1', {
-     *   body: Body.json({ email: 'contact@tauri.app' })
-     * });
-     * ```
-     */
-    patch<T>(url: string, options?: RequestOptions): Promise<Response<T>>;
-    /**
-     * Makes a DELETE request.
-     * @example
-     * ```typescript
-     * import { getClient } from '@tauri-apps/plugin-http';
-     * const client = await getClient();
-     * const response = await client.delete('http://localhost:3003/users/1');
-     * ```
-     */
-    delete<T>(url: string, options?: RequestOptions): Promise<Response<T>>;
-}
-/**
- * Creates a new client using the specified options.
  * @example
  * ```typescript
- * import { getClient } from '@tauri-apps/plugin-http';
- * const client = await getClient();
+ * const response = await fetch("http://my.json.host/data.json");
+ * console.log(response.status);  // e.g. 200
+ * console.log(response.statusText); // e.g. "OK"
+ * const jsonData = await response.json();
  * ```
- *
- * @param options Client configuration.
- *
- * @returns A promise resolving to the client instance.
  *
  * @since 2.0.0
  */
-declare function getClient(options?: ClientOptions): Promise<Client>;
-/**
- * Perform an HTTP request using the default client.
- * @example
- * ```typescript
- * import { fetch } from '@tauri-apps/plugin-http';
- * const response = await fetch('http://localhost:3003/users/2', {
- *   method: 'GET',
- *   timeout: 30,
- * });
- * ```
- */
-declare function fetch<T>(url: string, options?: FetchOptions): Promise<Response<T>>;
-export type { Duration, ClientOptions, Part, HttpVerb, HttpOptions, RequestOptions, FetchOptions, };
-export { getClient, fetch, Body, Client, Response, ResponseType, type FilePart, };
+export declare function fetch(input: URL | Request | string, init?: RequestInit & ClientOptions): Promise<Response>;
--- dist-js/index.mjs
+++ dist-js/index.mjs
@@ -2,380 +2,59 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: MIT
 /**
- * @since 2.0.0
- */
-var ResponseType;
-(function (ResponseType) {
-    ResponseType[ResponseType["JSON"] = 1] = "JSON";
-    ResponseType[ResponseType["Text"] = 2] = "Text";
-    ResponseType[ResponseType["Binary"] = 3] = "Binary";
-})(ResponseType || (ResponseType = {}));
-/**
- * The body object to be used on POST and PUT requests.
+ * Fetch a resource from the network. It returns a `Promise` that resolves to the
+ * `Response` to that `Request`, whether it is successful or not.
  *
- * @since 2.0.0
- */
-class Body {
-    /** @ignore */
-    constructor(type, payload) {
-        this.type = type;
-        this.payload = payload;
-    }
-    /**
-     * Creates a new form data body. The form data is an object where each key is the entry name,
-     * and the value is either a string or a file object.
-     *
-     * By default it sets the `application/x-www-form-urlencoded` Content-Type header,
-     * but you can set it to `multipart/form-data` if the Cargo feature `multipart` is enabled.
-     *
-     * Note that a file path must be allowed in the `fs` scope.
-     * @example
-     * ```typescript
-     * import { Body } from "@tauri-apps/plugin-http"
-     * const body = Body.form({
-     *   key: 'value',
-     *   image: {
-     *     file: '/path/to/file', // either a path or an array buffer of the file contents
-     *     mime: 'image/jpeg', // optional
-     *     fileName: 'image.jpg' // optional
-     *   }
-     * });
-     *
-     * // alternatively, use a FormData:
-     * const form = new FormData();
-     * form.append('key', 'value');
-     * form.append('image', file, 'image.png');
-     * const formBody = Body.form(form);
-     * ```
-     *
-     * @param data The body data.
-     *
-     * @returns The body object ready to be used on the POST and PUT requests.
-     *
-     * @since 2.0.0
-     */
-    static form(data) {
-        const form = {};
-        const append = (key, v) => {
-            if (v !== null) {
-                let r;
-                if (typeof v === "string") {
-                    r = v;
-                }
-                else if (v instanceof Uint8Array || Array.isArray(v)) {
-                    r = Array.from(v);
-                }
-                else if (v instanceof File) {
-                    r = { file: v.name, mime: v.type, fileName: v.name };
-                }
-                else if (typeof v.file === "string") {
-                    r = { file: v.file, mime: v.mime, fileName: v.fileName };
-                }
-                else {
-                    r = { file: Array.from(v.file), mime: v.mime, fileName: v.fileName };
-                }
-                form[String(key)] = r;
-            }
-        };
-        if (data instanceof FormData) {
-            for (const [key, value] of data) {
-                append(key, value);
-            }
-        }
-        else {
-            for (const [key, value] of Object.entries(data)) {
-                append(key, value);
-            }
-        }
-        return new Body("Form", form);
-    }
-    /**
-     * Creates a new JSON body.
-     * @example
-     * ```typescript
-     * import { Body } from "@tauri-apps/plugin-http"
-     * Body.json({
-     *   registered: true,
-     *   name: 'tauri'
-     * });
-     * ```
-     *
-     * @param data The body JSON object.
-     *
-     * @returns The body object ready to be used on the POST and PUT requests.
-     *
-     * @since 2.0.0
-     */
-    static json(data) {
-        return new Body("Json", data);
-    }
-    /**
-     * Creates a new UTF-8 string body.
-     * @example
-     * ```typescript
-     * import { Body } from "@tauri-apps/plugin-http"
-     * Body.text('The body content as a string');
-     * ```
-     *
-     * @param value The body string.
-     *
-     * @returns The body object ready to be used on the POST and PUT requests.
-     *
-     * @since 2.0.0
-     */
-    static text(value) {
-        return new Body("Text", value);
-    }
-    /**
-     * Creates a new byte array body.
-     * @example
-     * ```typescript
-     * import { Body } from "@tauri-apps/plugin-http"
-     * Body.bytes(new Uint8Array([1, 2, 3]));
-     * ```
-     *
-     * @param bytes The body byte array.
-     *
-     * @returns The body object ready to be used on the POST and PUT requests.
-     *
-     * @since 2.0.0
-     */
-    static bytes(bytes) {
-        // stringifying Uint8Array doesn't return an array of numbers, so we create one here
-        return new Body("Bytes", Array.from(bytes instanceof ArrayBuffer ? new Uint8Array(bytes) : bytes));
-    }
-}
-/**
- * Response object.
- *
- * @since 2.0.0
- * */
-class Response {
-    /** @ignore */
-    constructor(response) {
-        this.url = response.url;
-        this.status = response.status;
-        this.ok = this.status >= 200 && this.status < 300;
-        this.headers = response.headers;
-        this.rawHeaders = response.rawHeaders;
-        this.data = response.data;
-    }
-}
-/**
- * @since 2.0.0
- */
-class Client {
-    /** @ignore */
-    constructor(id) {
-        this.id = id;
-    }
-    /**
-     * Drops the client instance.
-     * @example
-     * ```typescript
-     * import { getClient } from '@tauri-apps/plugin-http';
-     * const client = await getClient();
-     * await client.drop();
-     * ```
-     */
-    async drop() {
-        return window.__TAURI_INVOKE__("plugin:http|drop_client", {
-            client: this.id,
-        });
-    }
-    /**
-     * Makes an HTTP request.
-     * @example
-     * ```typescript
-     * import { getClient } from '@tauri-apps/plugin-http';
-     * const client = await getClient();
-     * const response = await client.request({
-     *   method: 'GET',
-     *   url: 'http://localhost:3003/users',
-     * });
-     * ```
-     */
-    async request(options) {
-        const jsonResponse = !options.responseType || options.responseType === ResponseType.JSON;
-        if (jsonResponse) {
-            options.responseType = ResponseType.Text;
-        }
-        return window
-            .__TAURI_INVOKE__("plugin:http|request", {
-            clientId: this.id,
-            options,
-        })
-            .then((res) => {
-            const response = new Response(res);
-            if (jsonResponse) {
-                /* eslint-disable */
-                try {
-                    response.data = JSON.parse(response.data);
-                }
-                catch (e) {
-                    if (response.ok && response.data === "") {
-                        response.data = {};
-                    }
-                    else if (response.ok) {
-                        throw Error(`Failed to parse response \`${response.data}\` as JSON: ${e};
-              try setting the \`responseType\` option to \`ResponseType.Text\` or \`ResponseType.Binary\` if the API does not return a JSON response.`);
-                    }
-                }
-                /* eslint-enable */
-                return response;
-            }
-            return response;
-        });
-    }
-    /**
-     * Makes a GET request.
-     * @example
-     * ```typescript
-     * import { getClient, ResponseType } from '@tauri-apps/plugin-http';
-     * const client = await getClient();
-     * const response = await client.get('http://localhost:3003/users', {
-     *   timeout: 30,
-     *   // the expected response type
-     *   responseType: ResponseType.JSON
-     * });
-     * ```
-     */
-    async get(url, options) {
-        return this.request({
-            method: "GET",
-            url,
-            ...options,
-        });
-    }
-    /**
-     * Makes a POST request.
-     * @example
-     * ```typescript
-     * import { getClient, Body, ResponseType } from '@tauri-apps/plugin-http';
-     * const client = await getClient();
-     * const response = await client.post('http://localhost:3003/users', {
-     *   body: Body.json({
-     *     name: 'tauri',
-     *     password: 'awesome'
-     *   }),
-     *   // in this case the server returns a simple string
-     *   responseType: ResponseType.Text,
-     * });
-     * ```
-     */
-    async post(url, body, options) {
-        return this.request({
-            method: "POST",
-            url,
-            body,
-            ...options,
-        });
-    }
-    /**
-     * Makes a PUT request.
-     * @example
-     * ```typescript
-     * import { getClient, Body } from '@tauri-apps/plugin-http';
-     * const client = await getClient();
-     * const response = await client.put('http://localhost:3003/users/1', {
-     *   body: Body.form({
-     *     file: {
-     *       file: '/home/tauri/avatar.png',
-     *       mime: 'image/png',
-     *       fileName: 'avatar.png'
-     *     }
-     *   })
-     * });
-     * ```
-     */
-    async put(url, body, options) {
-        return this.request({
-            method: "PUT",
-            url,
-            body,
-            ...options,
-        });
-    }
-    /**
-     * Makes a PATCH request.
-     * @example
-     * ```typescript
-     * import { getClient, Body } from '@tauri-apps/plugin-http';
-     * const client = await getClient();
-     * const response = await client.patch('http://localhost:3003/users/1', {
-     *   body: Body.json({ email: 'contact@tauri.app' })
-     * });
-     * ```
-     */
-    async patch(url, options) {
-        return this.request({
-            method: "PATCH",
-            url,
-            ...options,
-        });
-    }
-    /**
-     * Makes a DELETE request.
-     * @example
-     * ```typescript
-     * import { getClient } from '@tauri-apps/plugin-http';
-     * const client = await getClient();
-     * const response = await client.delete('http://localhost:3003/users/1');
-     * ```
-     */
-    async delete(url, options) {
-        return this.request({
-            method: "DELETE",
-            url,
-            ...options,
-        });
-    }
-}
-/**
- * Creates a new client using the specified options.
  * @example
  * ```typescript
- * import { getClient } from '@tauri-apps/plugin-http';
- * const client = await getClient();
+ * const response = await fetch("http://my.json.host/data.json");
+ * console.log(response.status);  // e.g. 200
+ * console.log(response.statusText); // e.g. "OK"
+ * const jsonData = await response.json();
  * ```
- *
- * @param options Client configuration.
- *
- * @returns A promise resolving to the client instance.
  *
  * @since 2.0.0
  */
-async function getClient(options) {
-    return window
-        .__TAURI_INVOKE__("plugin:http|create_client", {
-        options,
-    })
-        .then((id) => new Client(id));
-}
-/** @internal */
-let defaultClient = null;
-/**
- * Perform an HTTP request using the default client.
- * @example
- * ```typescript
- * import { fetch } from '@tauri-apps/plugin-http';
- * const response = await fetch('http://localhost:3003/users/2', {
- *   method: 'GET',
- *   timeout: 30,
- * });
- * ```
- */
-async function fetch(url, options) {
-    var _a;
-    if (defaultClient === null) {
-        defaultClient = await getClient();
+async function fetch(input, init) {
+    const maxRedirections = init === null || init === void 0 ? void 0 : init.maxRedirections;
+    const connectTimeout = init === null || init === void 0 ? void 0 : init.maxRedirections;
+    // Remove these fields before creating the request
+    if (init) {
+        delete init.maxRedirections;
+        delete init.connectTimeout;
     }
-    return defaultClient.request({
-        url,
-        method: (_a = options === null || options === void 0 ? void 0 : options.method) !== null && _a !== void 0 ? _a : "GET",
-        ...options,
+    const req = new Request(input, init);
+    const buffer = await req.arrayBuffer();
+    const reqData = buffer.byteLength ? Array.from(new Uint8Array(buffer)) : null;
+    const rid = await window.__TAURI_INVOKE__("plugin:http|fetch", {
+        cmd: "fetch",
+        method: req.method,
+        url: req.url,
+        headers: Array.from(req.headers.entries()),
+        data: reqData,
+        maxRedirections,
+        connectTimeout,
     });
+    req.signal.addEventListener("abort", () => {
+        window.__TAURI_INVOKE__("plugin:http|fetch_cancel", {
+            rid,
+        });
+    });
+    const { status, statusText, url, headers } = await window.__TAURI_INVOKE__("plugin:http|fetch_send", {
+        rid,
+    });
+    const body = await window.__TAURI_INVOKE__("plugin:http|fetch_read_body", {
+        rid,
+    });
+    const res = new Response(Uint8Array.from(body), {
+        headers,
+        status,
+        statusText,
+    });
+    // url is read only but seems like we can do this
+    Object.defineProperty(res, "url", { value: url });
+    return res;
 }
 
-export { Body, Client, Response, ResponseType, fetch, getClient };
+export { fetch };
 //# sourceMappingURL=index.mjs.map
