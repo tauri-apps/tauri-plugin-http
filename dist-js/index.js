144461bd068300f7436db25c6c7ad952dbefdb21
asomethings <16171942+asomethings@users.noreply.github.com>
2025-04-30 16:53:26 +0000

    feat(http): add status code 101, 103 for null body (#2655)
    Committed via a GitHub action: https://github.com/tauri-apps/plugins-workspace/actions/runs/14759906874
    
    Co-authored-by: amrbashir <amrbashir@users.noreply.github.com>

--- dist-js/index.js
+++ dist-js/index.js
@@ -113,9 +113,9 @@ async function fetch(input, init) {
     const { status, statusText, url, headers: responseHeaders, rid: responseRid } = await invoke('plugin:http|fetch_send', {
         rid
     });
-    // no body for 204, 205 and 304
-    // see https://searchfox.org/mozilla-central/source/dom/fetch/Response.cpp#258
-    const body = [204, 205, 304].includes(status)
+    // no body for 101, 103, 204, 205 and 304
+    // see https://fetch.spec.whatwg.org/#null-body-status
+    const body = [101, 103, 204, 205, 304].includes(status)
         ? null
         : new ReadableStream({
             start: (controller) => {
